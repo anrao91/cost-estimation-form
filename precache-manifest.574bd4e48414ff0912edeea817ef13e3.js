5e1bb2ee861fd28ba79c8bfa10cfe44934d2438e
Anusha Rao <anusha91rao@gmail.com>
2020-06-30 22:55:10 +0530

    Updates

---
+++ precache-manifest.574bd4e48414ff0912edeea817ef13e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8f472d58551ec2f5e1cbf9978e139d",
+    "url": "/cost-estimation-form/index.html"
+  },
+  {
+    "revision": "001d551798c883d439b4",
+    "url": "/cost-estimation-form/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "56b955ef23ae4e9d2509",
+    "url": "/cost-estimation-form/static/css/main.4630408a.chunk.css"
+  },
+  {
+    "revision": "001d551798c883d439b4",
+    "url": "/cost-estimation-form/static/js/2.7eb63b9c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cost-estimation-form/static/js/2.7eb63b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b955ef23ae4e9d2509",
+    "url": "/cost-estimation-form/static/js/main.be66f6f0.chunk.js"
+  },
+  {
+    "revision": "a91dde630c41f9ad5f4d",
+    "url": "/cost-estimation-form/static/js/runtime-main.60c9f413.js"
+  },
+  {
+    "revision": "4e54dab81d46e7f674b7ddb0f63b6801",
+    "url": "/cost-estimation-form/static/media/main_background_bg.4e54dab8.jpg"
+  }
+]);
